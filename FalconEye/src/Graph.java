9ee569e87749366fc9dbc74010b3c64dbbf32355
lestath <lestath@localhost.localdomain>
2016-03-22 01:37:11 +0100

    zabawa obrazkami

--- FalconEye/src/Graph.java
+++ FalconEye/src/Graph.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -7,8 +8,12 @@ import java.awt.Graphics2D;
 
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Method;
 
 import javax.swing.JPanel;
+
+import org.imgscalr.Scalr;
 
 import com.github.sarxos.webcam.Webcam;
 public class Graph extends JPanel implements Runnable{
@@ -16,7 +21,7 @@ public class Graph extends JPanel implements Runnable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-    private Image IMG;
+    private BufferedImage IMG;
     private Webcam web;
     private int W,H;
     private boolean Running;
@@ -34,10 +39,23 @@ public class Graph extends JPanel implements Runnable{
 	
 	@Override
 	protected void paintComponent(Graphics g){
+		int rgb;
 		Graphics2D g2d =(Graphics2D) g;
 		IMG = web.getImage();
+		IMG = Scalr.resize(IMG,W,H,null);
 		g2d.drawImage(IMG, 0, 0,W,H,this);
-	}
+		for(int x=0;x<IMG.getWidth();x++){
+			for(int y =0; y<IMG.getHeight();y++){
+					rgb = IMG.getRGB(x, y);
+				//	System.out.println(rgb);
+					if(rgb >-16776432 && rgb <-16777186){
+						g2d.setColor(Color.red);
+						g2d.drawRect(x+10, y+10,10,10);
+						
+					}
+				}
+			}
+		}
 	
 	public void run(){
 		while(true){
